e6bf5f3de25f72e4725f059640ca462afd0f1991
hpcx365 <2975380349@qq.com>
2024-09-05 22:35:07 +0800

    commit

--- src/solve/Solve.java
+++ src/solve/Solve.java
@@ -13,17 +13,42 @@ public class Solve {
     public static final double BENCH_WIDTH = 0.30;
     public static final double EXTENT_LENGTH = 0.275;
     public static final double INIT_THETA = 16 * DPI;
+    public static final double HEAD_VELOCITY = 1.0;
     
-    public static Vector[] points(double t, Curve c) {
+    public static final double EPS = 1.0e-9;
+    
+    public static Vector[] points(double time, Curve curve) {
     
     }
     
-    public static double nextTheta(double theta, double chord, Curve c) {
-        double x0 = c.x(theta);
-        double y0 = c.y(theta);
-        double angle = c.tan(theta);
+    public static double nextTheta(double theta, double chord, Curve curve) {
+        double x0 = curve.x(theta);
+        double y0 = curve.y(theta);
+        double angle = curve.tan(theta);
         
-        double dt = DPI / 720.0; // 步长0.5度
+        double deltaTheta = DPI / 720.0; // 步长0.5度
+        double currentTheta = theta + deltaTheta;
+        while (curve.chord(theta, currentTheta) < chord) {
+            currentTheta += deltaTheta;
+        }
         
+        double left = theta;
+        double right = currentTheta;
+        for (; ; ) {
+            double middle = 0.5 * (left + right);
+            double chord1 = curve.chord(theta, middle);
+            if (equal(chord, chord1)) {
+                return middle;
+            }
+            if (chord < chord1) {
+                right = middle;
+            } else {
+                left = middle;
+            }
+        }
+    }
+    
+    public static boolean equal(double a, double b) {
+        return Math.abs(a - b) < EPS;
     }
 }
