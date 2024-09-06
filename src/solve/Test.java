174f64490e5bbc6f6c138d7f75fec51e64c5cd7a
hpcx365 <2975380349@qq.com>
2024-09-06 09:44:34 +0800

    commit

--- src/solve/Test.java
+++
@@ -1,9 +0,0 @@
-package solve;
-
-public class Test {
-    
-    public static void main(String[] args) {
-        Curve curve = new Curve(0.55);
-        System.out.println(curve.L(curve.toTheta(0.0)));
-    }
-}
